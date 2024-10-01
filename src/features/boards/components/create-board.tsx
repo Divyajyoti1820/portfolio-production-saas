0179726a57b23e6006fecad7a015d93733b30010
divyajyoti <divyajyoti1820@gmail.com>
2024-10-01 21:18:43 +0530

    [DEVELOP] : Board creation FORM + API completed

--- src/features/boards/components/create-board.tsx
+++ src/features/boards/components/create-board.tsx
@@ -17,14 +17,47 @@ import { Label } from "@/components/ui/label";
 import { Input } from "@/components/ui/input";
 
 import { useCreateBoard } from "@/features/boards/api/use-create-board";
+import { PlusIcon, Trash2Icon } from "lucide-react";
+import { cn } from "@/lib/utils";
 
 export const CreateBoard = () => {
   const [title, setTitle] = useState<string>("");
+  const [columns, setColumns] = useState<string[]>([""]);
+
+  /* TRIAL CODE */
+  const addColumnInputFunction = () => {
+    setColumns([...columns, ""]);
+  };
+  const checkColsLength = columns.length === 5;
+
+  const removeColumn = (index: number) => {
+    const newColumns = columns.filter((_, colIdx) => colIdx !== index);
+    setColumns(newColumns);
+  };
+
+  const handleColumChange = (index: number, value: string) => {
+    const newColumns = [...columns];
+    newColumns[index] = value;
+    setColumns(newColumns);
+  };
+  /* TRIAL CODE */
 
   /* Create Board Form Handler */
   const mutation = useCreateBoard();
   const onSubmitHandler = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+
+    console.log({ title, columns });
+
+    mutation.mutate(
+      { title, columns },
+      {
+        onSettled: () => {
+          setTitle("");
+          setColumns([""]);
+        },
+      }
+    );
   };
   /* Create Board Form Handler */
 
@@ -36,20 +69,65 @@ export const CreateBoard = () => {
           <DialogDescription>
             Create your new board and add columns in which you want to separate
             your tasks.
-            <strong className="text-white"> Max. 6 columns per board.</strong>
+            <strong className="text-white"> Max. 5 columns per board.</strong>
           </DialogDescription>
         </DialogHeader>
-        <form onSubmit={onSubmitHandler} className="space-y-2.5">
-          <Label htmlFor="title">Title</Label>
-          <Input
-            id="title"
-            value={title}
-            onChange={(e) => setTitle(e.target.value)}
-            placeholder="e.g. Developer Board, UI/UX Design, Meetings.."
-            required
-            minLength={3}
-            maxLength={16}
-          />
+        <form onSubmit={onSubmitHandler} className="space-y-4">
+          <div className="flex flex-col space-y-2.5">
+            <Label htmlFor="title">Title</Label>
+            <Input
+              id="title"
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+              placeholder="e.g. Developer Board, UI/UX Design, Meetings.."
+              required
+              minLength={3}
+              maxLength={20}
+            />
+          </div>
+          <div className="flex flex-col space-y-2.5">
+            <Label htmlFor="title">Columns</Label>
+            {columns.map((column, index) => (
+              <div
+                key={index}
+                className="flex items-center justify-center gap-x-3"
+              >
+                <div className="flex flex-col gap-y-1 w-full">
+                  <Input
+                    value={column}
+                    onChange={(e) => handleColumChange(index, e.target.value)}
+                    placeholder="e.g. Design, Backend, Frontend"
+                    required
+                    minLength={3}
+                    maxLength={16}
+                  />
+                  {index === 0 && (
+                    <p className="text-[10px] w-full text-muted-foreground">
+                      Necessary to have 1 column at least.
+                    </p>
+                  )}
+                </div>
+                <button
+                  onClick={() => removeColumn(index)}
+                  className={cn(
+                    "p-2 bg-destructive disabled:bg-destructive/60 text-white shrink-0 rounded-xl",
+                    index === 0 && "hidden"
+                  )}
+                >
+                  <Trash2Icon className="size-4" />
+                </button>
+              </div>
+            ))}
+          </div>
+          <Button
+            onClick={addColumnInputFunction}
+            disabled={checkColsLength}
+            size="sm"
+            className="flex ml-auto items-center justify-center gap-x-2 rounded-xl bg-primary/60 font-semibold"
+          >
+            <PlusIcon className="size-4" />
+            <span>Add new column</span>
+          </Button>
 
           <DialogFooter>
             <DialogClose>
